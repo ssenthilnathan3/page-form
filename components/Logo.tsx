70222eaab5def3668df3ef2d73c16568d93e015f
Senthilnathan <ssenthil1490@outlook.com>
2023-11-17 00:22:57 +0530

    initial commit

---
+++ components/Logo.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <Link href="/" className='font-bold text-3xl bg-gradient-to-r from-[#1FA2FF] to-[#12D8FA] text-transparent bg-clip-text items-center hover:cursor-pointer'>PageForm</Link>
+  )
+}
+
+export default Logo
